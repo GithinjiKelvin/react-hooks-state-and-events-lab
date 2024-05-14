3fc4cbf979215bd5b381863eaeae7f5bd463ccb1
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-05-14 23:00:49 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cName, setCName] = useState(false);
+
+  function changeClass(){
+    setCName((cName)=> !cName)
+  }
   return (
-    <li className="">
+    <li className={cName ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={changeClass} className="add">{cName ? "Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
